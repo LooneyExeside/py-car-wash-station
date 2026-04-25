b9031e05b2d92e0dff452e7f5ce735f90e3e5d1a
Looney .exe <dmytro1601@gmail.com>
2026-04-25 23:49:40 +0200

    Fix anotation and too long

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -23,10 +34,13 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         difference = self.clean_power - car.clean_mark
-        price = car.comfort_class * difference * self.average_rating / self.distance_from_city_center
+        price = (car.comfort_class
+                 * difference
+                 * self.average_rating
+                 / self.distance_from_city_center)
         return round(price, 1)
 
-    def wash_single_car(self, car: Car):
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
